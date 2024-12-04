77b43e7562ba5e275df4fd03edba982bac105a01
Muhammad Ibrahim <m.ibrahim99x@gmail.com>
2024-12-04 14:50:34 +0200

    Remove .env

--- easy-gen-be/src/app.module.ts
+++ easy-gen-be/src/app.module.ts
@@ -1,29 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     AuthModule,
-    TypeOrmModule.forRoot({
-      type: 'mongodb',
-      url: 'mongodb://0.0.0.0:27017/auth',
-
-      // Optional but recommended configurations
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-
-      // Entity directories
-      // entities: [__dirname + '/../**/*.entity.{js,ts}'],
-      autoLoadEntities: true,
-
-      // Optional: Enable synchronization (be careful in production)
-      synchronize: process.env.NODE_ENV !== 'production',
-
-      // Optional: Set logging
-      logging: process.env.NODE_ENV !== 'production',
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mongodb',
+        url: configService.get<string>('MONGODB_URL'),
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        autoLoadEntities: true,
+        synchronize: configService.get<string>('NODE_ENV') !== 'production',
+        logging: configService.get<string>('NODE_ENV') !== 'production',
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [],
